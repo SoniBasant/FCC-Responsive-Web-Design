34c2d70ab5b663192a98d4462259879800bfea7e
Basant Soni <94775800+SoniBasant@users.noreply.github.com>
2023-04-08 11:20:54 +0530

    Add files via upload
    Add HTML, CSS and JavaScript files

---
+++ A1. Survey Form Page/surveyForm.js
@@ -0,0 +1,6 @@
+// variables
+const content = document.getElementById('creditContent');
+const creditBtn = document.getElementById('creditBtn');
+// function to hide/display photo credits
+creditBtn.addEventListener('click', () => content.classList.toggle('active'));
+
