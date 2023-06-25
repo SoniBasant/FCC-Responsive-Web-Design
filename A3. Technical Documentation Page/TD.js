67046332113f1a6d49653f788fb981ca7c40afe5
Basant Soni <94775800+SoniBasant@users.noreply.github.com>
2023-06-25 16:04:43 +0530

    Add code to hide navbar by clicking on screen

--- A3. Technical Documentation Page/TD.js
+++ A3. Technical Documentation Page/TD.js
@@ -3,6 +3,7 @@
 const open_btn  = document.querySelector('.open-btn');
 const close_btn  = document.querySelector('.close-btn');
 const nav = document.querySelector('#navbar');
+const mainDoc = document.getElementById("main-doc")
 
 // open navbar
 open_btn.addEventListener('click', () => {
@@ -11,5 +12,10 @@ open_btn.addEventListener('click', () => {
 
 // close navbar
 close_btn.addEventListener('click', () => {
+    nav.classList.remove('visible');
+});
+
+// close navbar by clicking on the screen (other than navbar)
+mainDoc.addEventListener('click', () => {
     nav.classList.remove('visible');
 });
